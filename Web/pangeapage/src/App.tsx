69648af416c17ac03a5ca61f796112ec1158eb31
Cheyne Hayworth <104659633+ArielRin@users.noreply.github.com>
2024-01-27 13:17:31 +1100

    add columns to page

--- Web/pangeapage/src/App.tsx
+++ Web/pangeapage/src/App.tsx
@@ -6,8 +6,9 @@ import CountUp from 'react-countup';
 import "./styles/Home.css";
 
 const TOKEN_ADDRESS = '0xd8b9e0993fce7d05b3f11d828cf52d17637142ca'; //og pangea ca
+
 // import abiFile from './abiFile.json';
-const TOKEN_IMAGE = 'https://raw.githubusercontent.com/ArielRin/PangeaPage-Update/master/Web/pangeapage/src/pangearnd.png';
+const TOKEN_IMAGE = 'https://raw.githubusercontent.com/ArielRin/PangeaPage-Update/master/Web/pangeapage/src/images/pangeatoken.png';
 const TOKEN_SYMBOL = 'PRT';
 const TOKEN_DECIMALS = 18;
 
@@ -16,7 +17,7 @@ const INITIAL_SUPPLY = 1000000; //  set at 1,000,000
 import bannerImage from './images/banner1.png'; //
 import gregImage from './images/greg.jpg'; //
 import mattImage from './images/matt.jpg';
-import backgroundImage from './images/bkg.png';
+import backgroundImage from './images/bkg.gif';
 
 import logo3 from './images/3.png';
 import logo5 from './images/5.png';
@@ -34,6 +35,7 @@ import nft1 from './images/NFT_Pangea-1.jpg';
 import nft2 from './images/NFT_Pangea-2.jpg';
 import nft3 from './images/NFT_Pangea-3.jpg';
 
+import pangeaToken from './images/pangea.png';
 
 
 
@@ -46,6 +48,7 @@ export default function Home() {
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
       minHeight: '100vh',
+      backgroundPosition: 'center',
 
     };
 
@@ -272,26 +275,14 @@ useEffect(() => {
   <div style={appStyle}>
       <nav className="navbar">
         <ul className="nav-links">
-          <li><a href="#about">About</a></li>
           <li><a href="#tokenomics">Token</a></li>
           <li><a href="#nft">NFT</a></li>
           <li><a href="#team">Team</a></li>
-          <li><a href="#buynow">Lounge</a></li>
+          <li><a href="https://pancakeswap.finance/swap?outputCurrency=0xd8b9e0993fce7d05b3f11d828cf52d17637142ca&chainId=56">Buy Now</a></li>
         </ul>
       </nav>
       <div className="container">
 
-    <p><a href="https://github.com/ArielRin/PangeaPage-Update/blob/master/readme.md#to-do--next-update"
-       target="_blank"
-       rel="noopener noreferrer">
-      **** DRAFT ONLY ****
-    </a></p>
-
-  <p><a href="https://github.com/ArielRin/PangeaPage-Update/commits/master/"
-     target="_blank"
-     rel="noopener noreferrer">
-    Check progress here
-  </a></p>
 
 
         <div className="header">
@@ -310,70 +301,88 @@ useEffect(() => {
           </a>
         </div>
 
-
-        <div className="links">
-
-      {/* About Section */}
-      <section id="about" className="partner-section">
-          <p className="outlined-text2">
-
-          </p>
-          <p className="descriptionsml">
-          Welcome to Pangea, where exclusive rewards await you. Our Pangea Rewards Token (PRT) holders enjoy a unique 9% return in BNB, including an additional 1% bonus on each sale. This high-yield benefit is coupled with low tax implications, ensuring your gains are maximized without heavy tax burdens. Our exclusive lounge is more than just an investment; it's an elite experience.
-          Accessing these privileges is simple. Just own our exclusive NFT and hold a minimum of $50 in PRT. This small investment opens doors to higher BNB rewards and a smart financial journey in our community. Join us with a $50 PRT investment, secure your NFT, and start enjoying an enhanced 9% return in BNB on your transactions, all with the assurance of minimized taxes.
-          </p>
-      </section>
-
-      {/* Token Section */}
-      <section id="tokenomics" className="black-section">
-          <p className="outlined-text2">
-            Pangea Rewards Token
-          </p>
-          <p className="descriptiontoken">
-            Contract: <a href="#!" onClick={() => copyToClipboard(TOKEN_ADDRESS)}>0xd8b9e0993fce7d05b3f11d828cf52d17637142ca</a>
-          </p>
-              {copySuccess && <div>{copySuccess}</div>}
-          <p className="descriptiontoken">
-               Symbol: $PRT
-          </p>
-          <p className="descriptiontoken">
-               Current Value: ${tokenPriceUSD}
-          </p>
-          <p className="descriptiontoken">
-               Current Market Cap: {marketCap}
-          </p>
-          <p className="descriptiontoken">
-               Total Liquidity: {totalLiquidityUSD}
-          </p>
-          <p className="descriptionsml">
-              For purchases, enjoy a 6% allocation with 4% in BNB rewards, 1% contributed to liquidity, and 1% for buybacks and token burning (triggered at a 500-token threshold). On sales, benefit from a 7% distribution, including 4% in BNB rewards, 1% shared with NFT holders maintaining $50.00 in tokens, 1% towards liquidity, and another 1% for buybacks and burning, also activated at 500 tokens.
-          </p>
+     <div class="section-container">
+    <div class="column">
 
 
-          <p className="descriptionsmlnsapce">
-              Initial Supply: {INITIAL_SUPPLY.toLocaleString()}
-          </p>
-          <p className="descriptionsmlnsapce">
-             Remaining Supply: {totalSupply}
-          </p>
-          <p className="descriptionsmlnsapce">
-                     Tokens Removed from Supply: {tokensRemoved}
-          </p>
-          <button className="button2" onClick={addTokenToWallet}>
-            Add Token to Wallet
-          </button>
-          <button className="button" onClick={copyToClipboard}>Copy Address</button>
-            {copySuccess && <div>{copySuccess}</div>}
-     </section>
+          {/* About Section */}
+          <section id="about" className="partner-section">
+              <p className="outlined-text2">
+                 Welcome
+              </p>
+              <p className="descriptionsml">
+              Exclusive rewards await you. Our Pangea Rewards Token (PRT) holders enjoy a unique 9% return in BNB, including an additional 1% bonus on each sale. This high-yield benefit is coupled with low tax implications, ensuring your gains are maximized without heavy tax burdens. Our exclusive lounge is more than just an investment; it's an elite experience.
+              Accessing these privileges is simple. Just own our exclusive NFT and hold a minimum of $50 in PRT. This small investment opens doors to higher BNB rewards and a smart financial journey in our community. Join us with a $50 PRT investment, secure your NFT, and start enjoying an enhanced 9% return in BNB on your transactions, all with the assurance of minimized taxes.
+              </p>
 
-     <section id="tokenlinks" className="black-section">
-        <div className="link-container">
-          <p><a href="https://pancakeswap.finance/swap?outputCurrency=0xd8b9e0993fce7d05b3f11d828cf52d17637142ca&chainId=56" target="_blank" rel="noopener noreferrer">Buy Token</a></p>
-          <p><a href="https://bscscan.com/token/0xd8b9e0993fce7d05b3f11d828cf52d17637142ca#code" target="_blank" rel="noopener noreferrer">BSC Scan</a></p>
-          <p><a href="https://pancakeswap.finance/v2/add/BNB/0xd8b9e0993fce7d05b3f11d828cf52d17637142ca" target="_blank" rel="noopener noreferrer">Add to LP</a></p>
-          <p><a href="https://dexscreener.com/bsc/0xad80fdc107d983cd76bec153abc00ff00e3477de" target="_blank" rel="noopener noreferrer">Chart</a></p>
-        </div>
-     </section>
+              <div class="token-icon-container">
+  <img src={pangeaToken} alt="Token Icon" class="token-icon" />
+</div>
+
+          </section>
+    </div>
+
+
+
+    <div class="column">
+
+
+          <section id="tokenomics" className="black-section">
+              <p className="outlined-text2">
+                Pangea Rewards Token
+              </p>
+
+                   <p className="contract">
+                     <a href="#!" onClick={() => copyToClipboard(TOKEN_ADDRESS)}>0xd8b9e0993fce7d05b3f11d828cf52d17637142ca</a>
+                   </p>
+                       {copySuccess && <div>{copySuccess}</div>}
+
+              <p className="descriptiontoken">
+                   Symbol: $PRT
+              </p>
+              <p className="descriptiontoken">
+                   Current Value: ${tokenPriceUSD}
+              </p>
+              <p className="descriptiontoken">
+                   Current Market Cap: {marketCap}
+              </p>
+              <p className="descriptiontoken">
+                   Total Liquidity: {totalLiquidityUSD}
+              </p>
+              <p className="descriptionsml">
+                  For purchases, enjoy a 6% allocation with 4% in BNB rewards, 1% contributed to liquidity, and 1% for buybacks and token burning (triggered at a 500-token threshold). On sales, benefit from a 7% distribution, including 4% in BNB rewards, 1% shared with NFT holders maintaining $50.00 in tokens, 1% towards liquidity, and another 1% for buybacks and burning, also activated at 500 tokens.
+              </p>
+
+
+              <p className="descriptionsmlnsapce">
+                  Initial Supply: {INITIAL_SUPPLY.toLocaleString()}
+              </p>
+              <p className="descriptionsmlnsapce">
+                 Remaining Supply: {totalSupply}
+              </p>
+              <p className="descriptionsmlnsapce">
+                         Tokens Removed from Supply: {tokensRemoved}
+              </p>
+              <button className="button2" onClick={addTokenToWallet}>
+                Add Token to Wallet
+              </button>
+              <button className="button" onClick={copyToClipboard}>Copy Address</button>
+                {copySuccess && <div>{copySuccess}</div>}
+          </section>
+    </div>
+</div>
+
+
+
+<section id="tokenlinks" className="black-section">
+   <div className="link-container">
+     <p><a href="https://pancakeswap.finance/swap?outputCurrency=0xd8b9e0993fce7d05b3f11d828cf52d17637142ca&chainId=56" target="_blank" rel="noopener noreferrer">Buy Token</a></p>
+     <p><a href="https://bscscan.com/token/0xd8b9e0993fce7d05b3f11d828cf52d17637142ca#code" target="_blank" rel="noopener noreferrer">BSC Scan</a></p>
+     <p><a href="https://pancakeswap.finance/v2/add/BNB/0xd8b9e0993fce7d05b3f11d828cf52d17637142ca" target="_blank" rel="noopener noreferrer">Add to LP</a></p>
+     <p><a href="https://dexscreener.com/bsc/0xad80fdc107d983cd76bec153abc00ff00e3477de" target="_blank" rel="noopener noreferrer">Chart</a></p>
+   </div>
+</section>
+
 
      <div id="dexscreener-embed">
         <iframe
@@ -384,7 +393,7 @@ useEffect(() => {
       </div>
 
      {/* NFT Section */}
-     <section id="nft" className="partner-section">
+     <section id="nft" className="nft-section">
           <p className="outlined-text2 ">
             Pangea NFT Collection
           </p>
@@ -455,7 +464,6 @@ useEffect(() => {
 
     </div>
 
-    </div>
 
          {/* Partners Section */}
          <section id="footer" className="footer-section">
@@ -512,7 +520,25 @@ useEffect(() => {
                         }
 
                       />
+
+
                     </div>
+
+
+
+                        <p className="contract"><a href="https://github.com/ArielRin/PangeaPage-Update/blob/master/readme.md#to-do--next-update"
+                           target="_blank"
+                           rel="noopener noreferrer">
+                          **** DRAFT ONLY ****
+                        </a></p>
+
+                      <p className="contract"><a href="https://github.com/ArielRin/PangeaPage-Update/commits/master/"
+                         target="_blank"
+                         rel="noopener noreferrer">
+                        Check progress here
+                      </a></p>
+
+
                     <p className="contract">
                         <a href="https://t.me/InHausDevelopment/1"
                            target="_blank"
